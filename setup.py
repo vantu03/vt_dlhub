b600cd1c43d7fe63827678b1e081587e67325387
vantu03 <concaoheo203@gmail.com>
2025-05-28 08:11:42 +0700

    add media audio

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vt_dlhub",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "requests",
